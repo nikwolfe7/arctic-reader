5feb863a3f7060aa90f47d772a8f7205d620abd6
Cynthia Dokyi <cdokyi@Cynthias-Air.attlocal.net>
2022-07-24 23:56:07 -0400

    added questions for test

--- questions_data.js
+++ questions_data.js
@@ -1,19 +1,116 @@
 const questions = [
-  {
+{
+    "title": "Breathing Exercises",
+    "question_type": "simple",
+    "data": [
+        "Take 10 deep breaths.<br>Count them out loud.",
+        "Did you take the breaths?",
+        "Good job!",
+    ]
+},
+{
+    "title": "Incentive Spirometer",
+    "question_type": "simple",
+    "data": [
+        "Take a deep breath with the spirometer.",
+        "Take one more deep breath with the spirometer.",
+        "Take a third deep breath with the spirometer.",
+        "Great job!",
+        "Remember to do this 10x per hour."
+    ]
+},
+{
+    "title": "In Bed Physical Therapy: Legs",
+    "question_type": "simple",
+    "data": [
+        "Set 1",
+        "Knee raises left leg<br>(10x)",
+        "Knee raises right leg<br>(10x)",
+        "Ankle pumps, thrusting both feet down<br>(10x)",
+        "Alternating ankle pumps<br>(10x)",
+        "Press and hold right foot down<br>(5 seconds)",
+        "Press and hold left foot down<br>(5 seconds)",
+        "Set 2",
+        "Knee raises left leg<br>(10x)",
+        "Knee raises right leg<br>(10x)",
+        "Ankle pumps, thrusting both feet down<br>(10x)",
+        "Alternating ankle pumps<br>(10x)",
+        "Press and hold right foot down<br>(5 seconds)",
+        "Press and hold left foot down<br>(5 seconds)",
+        "Set 3",
+        "Knee raises left leg<br>(10x)",
+        "Knee raises right leg<br>(10x)",
+        "Ankle pumps, thrusting both feet down<br>(10x)",
+        "Alternating ankle pumps<br>(10x)",
+        "Press and hold right foot down<br>(5 seconds)",
+        "Press and hold left foot down<br>(5 seconds)",
+        "Awesome job!"
+    ]
+},
+{
+    "title": "Sitting Physical Therapy: Legs",
+    "question_type": "simple",
+    "data": [
+        "Set 1",
+        "Extend left leg (10x)",
+        "Extend right leg (10x)",
+        "Press ankles on the floor (10x)",
+        "Set 2",
+        "Extend left leg, hold 3 seconds (10x)",
+        "Extend right leg, hold 3 seconds (10x)",
+        "Press ankles on the floor (10x)",
+        "Set 3",
+        "Extend left leg (10x)",
+        "Extend right leg (10x)",
+        "Press ankles on the floor (10x)",
+        "Awesome job!"
+    ]
+},
+{
     "title": "Family Questions",
     "question_type": "simple",
     "data": [
-    "O L I V I A",
-      "What is your name?",
-      "What is your wife's name?"
+        "What is your name?",
+        "What is your wife's name?",
+        "What are your children's names?",
+        "What are your children's spouses names?",
+        "What are you niece and nephew's names?",
+        "What is your granddaughter's name?",
+        "What are the names of your cats?",
+        "What was the name of your first cat?",
+        "What are the names of all the cats you've ever had?",
     ]
-  },
-  {
+},
+{
+    "title": "General Knowledge",
+    "question_type": "simple",
+    "data": [
+        "What year is it?",
+        "What day of the week is it?",
+        "When is your birthday?",
+        "What month is it?",
+        "What's the date?",
+        "What day will it be tomorrow?",
+        "Who is the president of the United States?",
+        "Who is the prime minister of the United Kingdom?",
+    ]
+},
+{
     "title": "Sentence Completion",
     "question_type": "completion",
     "data": [
-      "The room is either too hot or too ____",
-      "My plants are either too short or too ____"
+        "The room is either too hot or too ____",
+        "My plants are either too short or too ____"
     ]
-  },
+},
+{
+    "title": "Speech therapy: say out loud",
+    "question_type": "simple",
+    "data": [
+        "Puh-tuh-kuh<br>Puh-tuh-kuh<br>Puh-tuh-kuh",
+        "Puh puh puh",
+        "Tuh tuh tuh",
+        "Kuh kuh kuh"
+    ]
+}, 
 ];
---
+++ test.js
@@ -0,0 +1,19 @@
+const questions = [
+  {
+    "title": "First",
+    "question_type": "simple",
+    "data": [
+      "1",
+      "2",
+      "3"
+    ]
+  },
+  {
+    "title": "Second",
+    "question_type": "completion",
+    "data": [
+      "1",
+      "2"
+    ]
+  },
+];
