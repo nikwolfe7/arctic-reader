d6790f82ebc9955d431a5bab5cd4500bd2949179
Cynthia Dokyi <cdokyi@Cynthias-Air.attlocal.net>
2022-07-25 01:39:20 -0400

    update

--- questions_data.js
+++ questions_data.js
@@ -101,6 +101,24 @@ const questions = [
     ]
 },
 {
+    "title": "Occupational Therapy: Right hand",
+    "question_type": "simple",
+    "data": [
+        "Open and close right hand.<br>(5x)",
+        "Squeeze red squishy ball.<br>(5x)",
+        "Give me a firm handshake.",
+        "Squeeze two fingers as hard as possible.",
+    ]
+},
+{
+    "title": "Occupational Therapy: Hand gestures",
+    "question_type": "simple",
+    "data": [
+        "👌<br>Dive &ldquo;OK&rdquo; sign",
+        "👍<br>Thumbs up sign",
+    ]
+},
+{
     "title": "Family Questions",
     "question_type": "simple",
     "data": [
