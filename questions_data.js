c17a2b915b9a3355bcb7e419622c744a05bb735b
Cynthia Dokyi <cdokyi@Cynthias-Air.attlocal.net>
2022-07-25 01:30:46 -0400

    update

--- questions_data.js
+++ questions_data.js
@@ -52,18 +52,52 @@ const questions = [
     "question_type": "simple",
     "data": [
         "Set 1",
-        "Extend left leg (10x)",
-        "Extend right leg (10x)",
-        "Press ankles on the floor (10x)",
+        "Extend left leg<br>(10x)",
+        "Extend right leg<br>(10x)",
+        "Press ankles on the floor<br>(10x)",
         "Set 2",
-        "Extend left leg, hold 3 seconds (10x)",
-        "Extend right leg, hold 3 seconds (10x)",
-        "Press ankles on the floor (10x)",
+        "Extend left leg, hold 3 seconds<br>(10x)",
+        "Extend right leg, hold 3 seconds<br>(10x)",
+        "Press ankles on the floor<br>(10x)",
         "Set 3",
-        "Extend left leg (10x)",
-        "Extend right leg (10x)",
-        "Press ankles on the floor (10x)",
-        "Awesome job!"
+        "Extend left leg<br>(10x)",
+        "Extend right leg<br>(10x)",
+        "Press ankles on the floor<br>(10x)",
+        "Woo! Awesome!"
+    ]
+},
+{
+    "title": "Physical Therapy: Right arm",
+    "question_type": "simple",
+    "data": [
+        "Set 1",
+        "Raise right arm as high as possible<br>(10x)",
+        "Right arm curl<br>(Dumbbell curl)<br>(10x)",
+        "Extend right arm and move wrist up, down, around<br>(Full range of motion)",
+        "&ldquo;Pulling your BC inflator&rdquo;<br>Reach right arm up to your shoulder and down.<br>(5x)",
+        "&ldquo;Reach for your sword&rdquo;<br>Reach right arm up to your shoulder and down.<br>(5x)",
+        "Set 2",
+        "Raise right arm as high as possible<br>(10x)",
+        "Right arm curl<br>(Dumbbell curl)<br>(10x)",
+        "Extend right arm and move wrist up, down, around<br>(Full range of motion)",
+        "&ldquo;Pulling your BC inflator&rdquo;<br>Reach right arm up to your shoulder and down.<br>(5x)",
+        "&ldquo;Reach for your sword&rdquo;<br>Reach right arm up to your shoulder and down.<br>(5x)",
+        "Set 3",
+        "Raise right arm as high as possible<br>(10x)",
+        "Right arm curl<br>(Dumbbell curl)<br>(10x)",
+        "Extend right arm and move wrist up, down, around<br>(Full range of motion)",
+        "&ldquo;Pulling your BC inflator&rdquo;<br>Reach right arm up to your shoulder and down.<br>(5x)",
+        "&ldquo;Reach for your sword&rdquo;<br>Reach right arm up to your shoulder and down.<br>(5x)",
+        "Done!"
+    ]
+},
+{
+    "title": "Physical Therapy: Arms together",
+    "question_type": "simple",
+    "data": [
+        "Place a towel or sheet on the table.<br>Placing both hands on sheet, move forward and backward.<br>(10x)",
+        "Both arms forward, fingers together.<br>Move arms together from your waist up over your head.<br>(5x)",
+        "Both arms forward, fingers together.<br>Move your wrists all the way to the right, left, and in a circle.<br>(5x)",
     ]
 },
 {
